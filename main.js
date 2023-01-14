7918ee41698b2e7aae14a082fc1ce713c5c9da38
Kelley-rosa <kelley.rosa@bellevuecollege.edu>
2023-01-13 16:37:28 -0800

    created calculate adverage functionality

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+/* WAIT FOR DOM */
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
+    // Button to Calculate Average
+    document.getElementById("average").addEventListener('click', calculateAverage);
 });
 
+/* FUNCTION DECLARATIONS */
+function calculateAverage() {
+    var num1 = parseInt(document.getElementById("input1").value);
+    var num2 = parseInt(document.getElementById("input2").value);
+    var num3 = parseInt(document.getElementById("input3").value);
+
+    // Calculate Total
+    var total = ((num1+num2+num3)/3);
+
+    // Display Total
+    document.getElementById("displayAverage").innerHTML = total;
+}
